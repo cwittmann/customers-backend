005f99278de57f6465e115f64196b2c789f9bda2
cwittmann <christofwittmann@web.de>
2020-04-15 15:56:34 +0200

    feat: return customer by id

--- server.js
+++ server.js
@@ -14,15 +14,27 @@ app.use(cors(corsOptions))
 app.listen(8000, ()=> { console.log("Server started!")});
 
 app.route('/api/customers').get((req, res) => {
-    res.send([{name: "Joe"}, {name: "Mike"}, {name: "George"}]        
+    res.send([{id: 1, name: "Joe"}, {id: 2, name: "Mike"}, {id:3, name: "George"}]        
     );
 });
 
-app.route('/api/customers/:customer').get((req,res) => {
+app.route('/api/customers/:id').get((req,res) => {   
     
-    const customerName = req.params['customer'];
+    const id = req.params['id'];
+    var customerName;
+
+    if(id == 1){
+        customerName = "Joe";
+    }
+    if(id == 2){
+        customerName = "Mike";
+    }
+    if(id == 3){
+        customerName = "George";
+    }
+
     res.send(
-        { name: customerName }
+        { id: id, name: customerName }
     );
 })
 
